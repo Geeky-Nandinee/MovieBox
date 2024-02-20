deeff5cfd4f6ba99f956b3ddf11f24a983d7beea
Nandinee <nandineepatel2003@gmail.com>
2024-02-20 15:14:05 +0530

    Commit by Nandinee on 20/2/2024

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/review_app')
+mongoose.connect('mongodb://127.0.0.1:27017/review_app')
 .then(() =>{
     console.log('db is connected!')
 })
