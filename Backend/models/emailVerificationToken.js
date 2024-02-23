d77178e4f3bb3688ba93f3384ed682bcc7a90e3c
Nandinee <nandineepatel2003@gmail.com>
2024-02-23 11:31:11 +0530

    Commited by Nandinee On 23/2/2024

--- Backend/models/emailVerificationToken.js
+++ Backend/models/emailVerificationToken.js
@@ -1,30 +1,32 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const emailVarificationTokenSchema = mongoose.Schema({
+const emailVerificationTokenSchema = mongoose.Schema({
     owner: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true
+        required: true,
     },
     token: {
         type: String,
         required: true,
     },
-    createAt:{
+    createdAt: {
         type: Date,
-        expired: 3600,
-        defaule: Date.now(),
+        expires: 3600,
+        default: Date.now(),
     },
 });
-emailVarificationTokenSchema.pre('save', async function
-(next){
-    if (this.isModified("token")){
-        this.token  = await bcrypt.hash(this.password, 10);
+
+emailVerificationTokenSchema.pre("save", async function (next) {
+    if (this.isModified("token")) {
+        this.token = await bcrypt.hash(this.token, 10);
     }
+
     next();
 });
 
 module.exports = mongoose.model(
     "EmailVerificationToken",
-    emailVarificationTokenSchema);
+    emailVerificationTokenSchema
+);
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,28 +1,31 @@
-const mongoose = require ("mongoose");
-const bcrypt = require("bcrypt");
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        trim : true,
+    name: {
+        type: String,
+        trim: true,
         required: true
     },
-    email:{
-        type:String,
-        trim : true,
+    email: {
+        type: String,
+        trim: true,
         required: true,
         unique: true
     },
-    password:{
-        type:String,
-        required: true,
-    },
-});
-userSchema.pre('save', async function(next){
-    if (this.isModified("password")){
-        this.password  = await bcrypt.hash(this.password, 10);
+    password: {
+        type: String,
+        required: true
     }
+})
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+
     next();
 });
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model('User', userSchema)
+
