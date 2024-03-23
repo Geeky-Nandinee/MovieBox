37eb1a774dc9dae16171b2948435337f65dd826d
Nandinee Patel <134035683+Geeky-Nandinee@users.noreply.github.com>
2024-03-23 16:08:01 +0530

    Commited by Nandinee Patel on 23/03/2024 - Uploading Actor Data & Image. Updating the Image Later Option

--- Backend/controllers/actor.js
+++ Backend/controllers/actor.js
@@ -2,15 +2,30 @@ const Actor = require("../models/actor");
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.djedxiciy,
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
   secure: true,
 });
 
-exports.createActor = (req, res) => {
+exports.createActor = async (req, res) => {
   const { name, about, gender } = req.body;
   const { file } = req;
 
   const newActor = new Actor({ name, about, gender });
+
+  if (file) {
+    const { secure_url, public_id } = await cloudinary.uploader.upload(
+      file.path
+    );
+    newActor.avatar = { url: secure_url, public_id };
+  }
+  await newActor.save();
+  res.status(201).json({
+    id: newActor._id,
+    name,
+    about,
+    gender,
+    avatar: newActor.avatar?.url,
+  });
 };
