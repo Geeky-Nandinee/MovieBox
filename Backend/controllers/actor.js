8e64aa92bb3d6b796fe11c70174f9ed10040cb88
Smit Modi <134035683+Geeky-Nandinee@users.noreply.github.com>
2024-03-23 15:37:24 +0530

    Commited by Nandinee Patel on 23/03/2024 - Setting Up Cloudinary

--- Backend/controllers/actor.js
+++ Backend/controllers/actor.js
@@ -1,3 +1,16 @@
+const Actor = require("../models/actor");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.djedxiciy,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+  secure: true,
+});
+
 exports.createActor = (req, res) => {
-  console.log(req.body);
+  const { name, about, gender } = req.body;
+  const { file } = req;
+
+  const newActor = new Actor({ name, about, gender });
 };
