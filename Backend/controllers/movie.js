34b43eb74d0db7a4b04553d81e545af3bbfc3cb8
Nandinee Patel <134035683+Geeky-Nandinee@users.noreply.github.com>
2024-03-24 11:59:08 +0530

    Commited by Nandinee Patel on 24/03/2024 - Uploading Poster With Multiple Sizes

--- Backend/controllers/movie.js
+++ Backend/controllers/movie.js
@@ -62,4 +62,22 @@ exports.createMovie = async (req, res) => {
 
     newMovie.writers = writers;
   }
+
+  // uploading poster
+  const cloudRes = await cloudinary.uploader.upload(file.path, {
+    transformation: {
+      width: 1280,
+      height: 720,
+    },
+    responsive_breakpoints: {
+      create_derived: true,
+      max_width: 640,
+      max_images: 3,
+    },
+  });
+
+  console.log(cloudRes);
+  console.log(cloudRes.responsive_breakpoints[0].breakpoints);
+
+  res.send("ok");
 };
--- Backend/utils/helper.js
+++ Backend/utils/helper.js
@@ -38,3 +38,14 @@ exports.formatActor = (actor) => {
     avatar: avatar?.url,
   };
 };
+
+exports.parseData = (req, res, next) => {
+  const { trailer, cast, genres, tags, writers } = req.body;
+  if (trailer) req.body.trailer = JSON.parse(trailer);
+  if (cast) req.body.cast = JSON.parse(cast);
+  if (genres) req.body.genres = JSON.parse(genres);
+  if (tags) req.body.tags = JSON.parse(tags);
+  if (writers) req.body.writers = JSON.parse(writers);
+
+  next();
+};
