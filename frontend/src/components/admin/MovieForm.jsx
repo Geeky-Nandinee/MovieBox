b28a8d952d8b375be7f41ac163a3a390aa6448f4
Nandinee Patel <134035683+Geeky-Nandinee@users.noreply.github.com>
2024-03-24 14:56:14 +0530

    Commited by Nandinee Patel on 24/03/2024 -Live Search Field
    Live Search Field:
    
    1. Input For Live Search
    2. Live Search Result Container
    3. Live Search Result Item
    4. Show & Hide Search Results
    5. Moving Selection Up & Down
    6. Creating Infinite Selection Loop
    7. Scroll Into Result View
    8. Reset Selection Index
    9. Select Result On Enter Press
    10. Fixing onClick on Results
    11. Refactor Live Search Component
    
    Co-Authored-By: Smit Modi <98140609+MojoSmit07@users.noreply.github.com>

--- frontend/src/components/admin/MovieForm.jsx
+++ frontend/src/components/admin/MovieForm.jsx
@@ -1,11 +1,59 @@
 import React from "react";
+import { commonInputClasses } from "../../utils/theme";
+import LiveSearch from "../LiveSearch";
 import TagsInput from "../TagsInput";
 
-export const commonInputClasses =
-  "w-full bg-transparent outline-none dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary transition dark:text-white text-primary";
+export const results = [
+  {
+    id: "1",
+    avatar:
+      "https://images.unsplash.com/photo-1643713303351-01f540054fd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "John Doe",
+  },
+  {
+    id: "2",
+    avatar:
+      "https://images.unsplash.com/photo-1643883135036-98ec2d9e50a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Chandri Anggara",
+  },
+  {
+    id: "3",
+    avatar:
+      "https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Amin RK",
+  },
+  {
+    id: "4",
+    avatar:
+      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Edward Howell",
+  },
+  {
+    id: "5",
+    avatar:
+      "https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Amin RK",
+  },
+  {
+    id: "6",
+    avatar:
+      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Edward Howell",
+  },
+];
+
 export default function MovieForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
+  };
+
+  const renderItem = (result) => {
+    return (
+      <div className="flex rounded overflow-hidden">
+        <img src={result.avatar} alt="" className="w-16 h-16 object-cover" />
+        <p className="dark:text-white font-semibold">{result.name}</p>
+      </div>
+    );
   };
 
   return (
@@ -30,7 +78,17 @@ export default function MovieForm() {
           ></textarea>
         </div>
 
-        <TagsInput />
+        <div>
+          <Label htmlFor="tags">Tags</Label>
+          <TagsInput />
+        </div>
+
+        <LiveSearch
+          results={results}
+          placeholder="Search profile"
+          renderItem={renderItem}
+          onSelect={(result) => console.log(result)}
+        />
       </div>
       <div className="w-[30%] h-5 bg-blue-400"></div>
     </form>
