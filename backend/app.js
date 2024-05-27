22cbd2ee470aeac17f419425324895feb568a514
Smit Modi <smitmodi21@gnu.ac.in>
2024-05-27 12:43:55 +0530

    changes in package.json and app.js
    Co-Authored-By: Nandinee Patel <134035683+Geeky-Nandinee@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,6 @@ app.use("/api/movie", movieRouter);
 app.use("/api/review", reviewRouter);
 app.use("/api/admin", adminRouter);
 app.use("/*", handleNotFound);
-//using this
 app.use(errorHandler);
 
 // app.post("/sign-in",
@@ -36,6 +35,7 @@ app.use(errorHandler);
 //     res.send("<h1>Hello I am from your backend about</h1>");
 //   });
 
-app.listen(8000, () => {
-  console.log("the port is listening on port 8000");
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+  console.log("the port is listening on port " + PORT);
 });
