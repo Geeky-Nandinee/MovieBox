2ba1010cacac98319fc883209442f8a398162132
Smit Modi <smitmodi21@gnu.ac.in>
2024-05-27 13:48:26 +0530

    Changing Base URL
    Co-Authored-By: Nandinee Patel <134035683+Geeky-Nandinee@users.noreply.github.com>

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://movie-box-backend-p41im9m2e-nandinee-patels-projects.vercel.app/api",
+  // baseURL: "http://localhost:8000/api",
 });
 
 export default client;
